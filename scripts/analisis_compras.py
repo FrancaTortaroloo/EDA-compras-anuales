6aec0f6f68b2bcabf508b5f0ed1fd488c59e6ffa
Franca Tortarolo <155097175+FrancaTortaroloo@users.noreply.github.com>
2025-01-17 20:43:08 +0100

    actualizacion, inicio de pipeline

---
+++ scripts/analisis_compras.py
@@ -0,0 +1,29 @@
+#%%
+import functions as func
+
+#%%ç
+print('Importar xslxs y convertir a DF')
+#%%
+nombre_archivo = ["Presupuesto_mensual_2021.xlsx", "Presupuesto_mensual_2022.xlsx", "Presupuesto_mensual_2023.xlsx", "Presupuesto_mensual_2024.xlsx"]
+rango_hojas = list(range(1,13))
+
+df = func.excel_a_df(nombre_archivo, rango_hojas)
+#%%
+#eliminar columnas que no voy a ocupar
+columnas = [0,4,7,8,9]
+df_completo = func.eliminar_columnas(df, columnas)
+#verificar que se han eliminado las columnas
+df_completo
+#%%
+#ponerle nombre a las columnas
+nom_col =['Fecha', 'Precio', 'Producto', 'Lugar', 'Categoria']
+df_completo = func.renombrar_col(df_completo, nom_col)
+df_completo.sample()
+# %%
+#verificar los tipos de datos y hacer cambios donde se requiera
+df_completo = fun.tipos_datos(df_completo)
+# %%
+#convertir la columna precio de object a float
+df_completo['Precio'] = pd.to_numeric(df_completo['Precio'], errors='coerce')
+df_completo['Precio'].dtype
+# %%
---
+++ scripts/functions.py
@@ -0,0 +1,40 @@
+#%%
+import matplotlib.pyplot as plt
+import numpy as np
+import openpyxl
+import os
+import pandas as pd
+import re
+import seaborn as sns
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning, module="openpyxl")
+
+#%%
+def excel_a_df(archivo, rango_hojas):
+    df_compras = pd.DataFrame() #df vacio para almacenar los resultados
+    for a in archivo:
+        try:
+            datos = pd.read_excel(a, sheet_name= rango_hojas, header= None, skiprows= 5)
+            #concatenar todas las hojas de los arhicvos en el df final
+            for hoja, df in datos.items():
+                df_compras = pd.concat([df_compras, df], ignore_index= True) #directamente concatena
+            #manejo de errores
+        except ValueError as e:
+            #print(f"Algunas hojas especificadas no existen en {archivo}: {e}")
+            continue
+        except Exception as e:
+            #print(f"Error inesperado al procesar el libro {a} y la hoja {l}")
+            continue
+    return df_compras 
+
+#%%
+def eliminar_columnas(df, col):
+    df_completo = df.drop(col, axis = 1)
+    return df_completo
+#%%
+def renombrar_col(df, nombre):
+    df.columns = nombre
+    return df
+#%%
+def tipos_datos(df):
+    print(df.dtypes)
